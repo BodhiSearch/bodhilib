c0414f552cc67488c4079a14fb690a298155cd33
anagri <127566+anagri@users.noreply.github.com>
2023-09-08 17:09:45 +0530

    [Amir] fixing test on CI, sorting docs by filename before comparing

--- tests/unit/data_loader/test_file_loader.py
+++ tests/unit/data_loader/test_file_loader.py
@@ -32,39 +32,37 @@ def test_file_loader_recursive_true(file_loader, tmp_loader_dir):
     file_loader.add_resource(dir=tmp_loader_dir, recursive=True)
 
     # iterate over documents and check if they are loaded correctly
-    dociter = iter(file_loader)
-    doc = next(dociter)
+    docs = list(iter(file_loader))
+    assert len(docs) == 3
+    docs = sorted(docs, key=lambda x: x.metadata["filename"])
+    doc = docs[0]
     assert doc.text == "hello world!"
     assert doc.metadata["filename"] == "test1.txt"
     assert doc.metadata["dirname"] == tmp_loader_dir
-    doc = next(dociter)
+    doc = docs[1]
     assert doc.text == "world hello!"
     assert doc.metadata["filename"] == "test2.txt"
     assert doc.metadata["dirname"] == tmp_loader_dir
-    doc = next(dociter)
+    doc = docs[2]
     assert doc.text == "hey world!"
     assert doc.metadata["filename"] == "test3.txt"
     assert doc.metadata["dirname"].startswith(tmp_loader_dir + "/tmpdir2")
-
-    with pytest.raises(StopIteration):
-        next(dociter)
 
 
 def test_file_loader_loads_recursive_false(file_loader, tmp_loader_dir):
     file_loader.add_resource(dir=tmp_loader_dir, recursive=False)
 
-    dociter = iter(file_loader)
-    doc = next(dociter)
+    docs = list(iter(file_loader))
+    assert len(docs) == 2
+    docs = sorted(docs, key=lambda x: x.metadata["filename"])
+    doc = docs[0]
     assert doc.text == "hello world!"
     assert doc.metadata["filename"] == "test1.txt"
     assert doc.metadata["dirname"] == tmp_loader_dir
-    doc = next(dociter)
+    doc = docs[1]
     assert doc.text == "world hello!"
     assert doc.metadata["filename"] == "test2.txt"
     assert doc.metadata["dirname"] == tmp_loader_dir
-
-    with pytest.raises(StopIteration):
-        next(dociter)
 
 
 def test_file_loader_loads_given_files(file_loader, tmp_loader_dir):
@@ -83,6 +81,7 @@ def test_file_loader_loads_method(file_loader, tmp_loader_dir):
     file_loader.add_resource(dir=tmp_loader_dir, recursive=True)
     docs = file_loader.load()
     assert len(docs) == 3
+    docs = sorted(docs, key=lambda x: x.metadata["filename"])
     doc = docs[0]
     assert doc.text == "hello world!"
     assert doc.metadata["filename"] == "test1.txt"
