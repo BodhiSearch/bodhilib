0beb74da1c1533d25e67e4a9cd167916bc813d79
anagri <127566+anagri@users.noreply.github.com>
2023-10-15 10:25:16 +0530

    [Amir] enhancing make.py with argparse to extend for other usecase

--- make.py
+++ make.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+import argparse
 import contextlib
 import os
 import subprocess
@@ -31,40 +33,67 @@ def get_directories(arg: str) -> List[str]:
     return dirs
 
 
-def execute_command(run: str, dirs: List[str], command_args: List[str]) -> int:
+def execute_command(dirs: List[str], command: List[str]) -> int:
     for dir_path in dirs:
-        cmd: List[str] = ["poetry"]
-        if run == "run":
-            cmd.append("run")
-        cmd.extend(command_args)
         with change_dir(dir_path):
-            print(f"Executing: {' '.join(cmd)} in directory {dir_path}...")
-            result = subprocess.run(cmd)
+            print(f"Executing: {' '.join(command)} in directory {dir_path}...")
+            result = subprocess.run(command)
             if result.returncode != 0:
                 return result.returncode
     return 0
 
 
+def main() -> None:
+    dir_opts = ["core", "all"]
+    plugin_names = [d.replace("bodhiext.", "") for d in os.listdir("plugins") if d.startswith("bodhiext.")]
+    dir_opts.extend(plugin_names)
+
+    parser = argparse.ArgumentParser(description="Python library to automate and orchestrate bodhilib build process.")
+    subparsers = parser.add_subparsers(dest="top_command", required=True)
+    # 'run' command
+    run_parser = subparsers.add_parser("run", help="Run command")
+    run_parser.add_argument(
+        "target",
+        type=str,
+        choices=dir_opts,
+        nargs="?",
+        default="all",
+        help="Project to run the `poetry run` command. E.g. core, all, common, openai etc.",
+    )
+    run_parser.add_argument("command", type=str, help="Poetry command to run. E.g. install etc.")
+    run_parser.add_argument(
+        "other_args",
+        type=str,
+        nargs=argparse.REMAINDER,
+        default=[],
+        help="Pass through arguments for the `poetry run` command.",
+    )
+    # 'exec' command
+    exec_parser = subparsers.add_parser("exec", help="Execute command")
+    exec_parser.add_argument(
+        "target",
+        type=str,
+        choices=dir_opts,
+        nargs="?",
+        default="all",
+        help="Project to execute `poetry` command. E.g. core, all, common, openai etc.",
+    )
+    exec_parser.add_argument("command", type=str, help="Command to execute")
+    exec_parser.add_argument(
+        "other_args", type=str, nargs=argparse.REMAINDER, default=[], help="Passthrough arguments for `poetry` command."
+    )
+    args = parser.parse_args()
+    # Check if target directory exists in plugins
+    if not os.path.exists(f"plugins/bodhiext.{args.target}") and args.target not in ["all", "core"]:
+        print(f"Error: Directory for target '{args.target}' does not exist: plugins/bodhiext.{args.target}")
+        sys.exit(1)
+
+    dirs = get_directories(args.target)
+    if args.top_command == "run":
+        sys.exit(execute_command(dirs, ["poetry", "run", args.command] + args.other_args))
+    elif args.top_command == "exec":
+        sys.exit(execute_command(dirs, ["poetry", args.command] + args.other_args))
+
+
 if __name__ == "__main__":
-    all_args = sys.argv
-    all_args = [arg for arg in all_args if arg != ""]
-    print("Running: python", all_args)
-    if len(all_args) < 3:
-        print("Usage: python make.py <run|exec> [<directory>] <command> [<other args>]")
-        sys.exit(1)
-    run_or_exec = all_args[1]
-    if run_or_exec not in ["run", "exec"]:
-        print("Usage: python make.py <run|exec> [<directory>] <command> [<other args>]")
-        print(f"argument='{run_or_exec}' is not one of 'run' or 'exec'")
-        sys.exit(1)
-    may_be_dir = all_args[2]
-    if may_be_dir in ["all", "core"] or os.path.exists(f"plugins/bodhiext.{may_be_dir}"):
-        if len(all_args) < 4:
-            print("Usage: python make.py <run|exec> [<directory>] <command> [<other args>]")
-            print("<command> not provided")
-            sys.exit(1)
-        dirs = get_directories(may_be_dir)
-        sys.exit(execute_command(run_or_exec, dirs, all_args[3:]))
-    else:
-        dirs = get_directories("all")
-        sys.exit(execute_command(run_or_exec, dirs, all_args[2:]))
+    main()
